722b8ff7fdc49c78ce01efde62603a506e2c9af4
Jordy3D <fchetar@gmail.com>
2023-10-18 15:12:54 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# dont
+online etiquette  
+
+A little bit of online etiquette for the modern age.
+
+## TODO
+- [ ] Replace images and content with wholly original content
+- [ ] Add more etiquette behaviours
+- [ ] Add more example conversations (that may load randomly)
+
+## Credits
+Images are from The Office, hosted by [NoHello](https://www.nohello.net/).  
+Don't Say Hello text is also from NoHello.
---
+++ js/script.js
@@ -0,0 +1,46 @@
+function createMessage(name, text, time, imageUrl) {
+    var messageHTML = `
+    <div class="message">
+        <div class="image">
+            <img src="${imageUrl}" alt="${name}" width="64" height="64">
+        </div>
+        <div class="message-details">
+            <div class="top">
+                <span class="name">${name}</span>
+                <span class="time">${time}</span>
+            </div>
+            <span class="text">
+                ${text}
+            </span>
+        </div>
+    </div>
+    `;
+
+    return messageHTML;
+}
+
+function createMessages(doMessages, dontMessages) {
+    // add the doMessages to the do .discord, and the dontMessages to the dont .discord
+    var dontDiscord = document.querySelector(".dont .discord");
+    var doDiscord = document.querySelector(".do .discord");
+
+    // print the messages in order, with a delay between each one
+    var delay = 0;
+    dontMessages.forEach(function (message) {
+        delay += message.delay;
+        setTimeout(function () {
+            dontDiscord.innerHTML += createMessage(message.name, message.text, message.time, message.imageUrl);
+        }, delay * 1000);
+    });
+
+    delay = 0;
+    doMessages.forEach(function (message) {
+        delay += message.delay;
+        setTimeout(function () {
+            doDiscord.innerHTML += createMessage(message.name, message.text, message.time, message.imageUrl);
+        }, delay * 1000);
+    });
+}
+
+// when page has loaded, create the messages
+// document.addEventListener("DOMContentLoaded", createMessages);
